38a3b7e68f9a57ab366e2c77fe72698ef154c074
haeseul <ksw06205@naver.com>
2022-03-17 13:14:38 +0900

    test

--- _posts/test.js
+++ _posts/test.js
@@ -1,1 +1,9 @@
-console.log(Number.MAX_SAFE_INTEGER);
+function sumTotal(...numbers) {
+    return numbers.reduce(function(total, current) {
+        return total + current
+    }, 0);   // 0은 total의 초기값
+}
+
+console.log(sumTotal(1,2,3,4,5,6,7));
+
+console.log(1);
