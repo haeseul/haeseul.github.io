c7e16c805baf4f2b67d0c7a6c0801ea4f7e344fc
haeseul <ksw06205@naver.com>
2022-03-18 13:06:57 +0900

    closure and debounce

--- _posts/test.js
+++ _posts/test.js
@@ -1,29 +1,14 @@
-// Super Class
-class Animal {
-    constructor(name, sound) {
-        this.name = name;
-        this.sound = sound;
-    }
+buttonElement.addEventListener(
+    'click',
+    debounce(handleCilck, 500),
+);
 
-    getInfo() {
-    return this.name + '가(이) ' + this.sound + ' 소리를 낸다.'
-    }
+function debounce(func, timeout = 300) {
+    let timer;
+
+    return (...args) => {
+        clearTimeout(timer);
+
+        timer = setTimeout(() => { func.apply(this, args); }, timeout)
+    };
 }
-
-class Friends extends Animal {
-    constructor(name, sound) {
-        super(name, sound);      // 부모의 생성자 함수 호출
-    }
-}
-
-const dog = new Friends('개', '멍멍')
-const cat = new Friends('고양이', '야옹')
-
-console.log(dog.getInfo())
-console.log(cat.getInfo())
-
-console.log(dog.constructor.name)   // Frineds
-console.log(cat.constructor.name)   // Frineds
-
-console.log(dog instanceof Friends) // true
-console.log(dog instanceof Animal) // true
