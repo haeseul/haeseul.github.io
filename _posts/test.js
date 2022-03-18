3e829cac42db5bea31805f84d822e65c44579afe
haeseul <ksw06205@naver.com>
2022-03-18 15:42:37 +0900

    async, callback, promise

--- _posts/test.js
+++ _posts/test.js
@@ -1,14 +1,32 @@
-buttonElement.addEventListener(
-    'click',
-    debounce(handleCilck, 500),
-);
+const starbucks = function (coffeeName) {
+    return new Promise((resolve, reject) => {
+        if (coffeeName === '아메리카노') {
+            resolve ('아메리카노 한잔입니다.');
+        } else {
+            reject ('아메리카노는 없습니다.');
+        }
+    });
+};
 
-function debounce(func, timeout = 300) {
-    let timer;
+// starbucks(drink)
+//     .then((res) => console.log(res))
+//     .catch((rej) => console.log(rej))           // 아메리카노는 없습니다.
+//     .finally(() => console.log('감사합니다.'));  // 감사합니다.
 
-    return (...args) => {
-        clearTimeout(timer);
+// starbucks('아메리카노')
+//     .then((res) => console.log(res))            // 아메리카노는 한잔입니다.
+//     .catch((rej) => console.log(rej))
+//     .finally(() => console.log('감사합니다.'));  // 감사합니다.
 
-        timer = setTimeout(() => { func.apply(this, args); }, timeout)
-    };
+async function americano(drink) {
+    try {
+        const result = await starbucks(drink);
+        return result
+    } catch (error) {
+        console.log(error)
+    } finally {
+        console.log('감사합니다.')
+    }
 }
+
+console.log(americano('아메리노'))
